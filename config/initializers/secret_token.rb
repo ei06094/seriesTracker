71e7b4243a215429842657ce82ae31f6f1408591
Jose Barbosa <jpedrombarbosa@gmail.com>
2011-11-03 10:28:23 +0000

    projecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeriesTracker::Application.config.secret_token = '2c445e44306506c6126384046d6c31837a854b74a7ab6da2efd16a47b6db8c9a09b3cb5d12bfa9b44392af1ccaddd5af24f04c4ed69dfeeedb093ebc7110452f'
